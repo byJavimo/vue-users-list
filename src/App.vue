8a8b3469faeea413395c6e5cc5ae651d6256bcf7
Javier Mora <javier.mora@pfsgroups-MacBook-Pro.local>
2021-06-15 11:32:15 +0200

    Project scaffolding

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <CreateUser msg="Welcome to Your Vue.js App"/>
+  <UsersList></UsersList>
+</template>
+
+<script>
+import CreateUser from './components/CreateUser.vue'
+import UsersList from './components/UsersList.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    CreateUser,
+    UsersList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
